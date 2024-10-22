ff9ad0b680140434bdf3447dd12aa779287124a8
Winlypse <nts3@o2.pl>
2024-10-22 15:24:52 +0200

    Changes!

--- src/main/kotlin/winlyps/noVinesSpread/NoVinesSpread.kt
+++ src/main/kotlin/winlyps/noVinesSpread/NoVinesSpread.kt
@@ -6,9 +6,14 @@ class NoVinesSpread : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("doVinesSpread", "false")
+        }
+        logger.info("NoVinesSpread plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoVinesSpread plugin has been disabled.")
     }
 }
