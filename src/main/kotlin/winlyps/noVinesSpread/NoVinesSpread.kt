e7deb3c017ab06abc28a3dcc2b6919c5e7523b62
Winlypse <nts3@o2.pl>
2024-10-22 15:24:38 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noVinesSpread/NoVinesSpread.kt
@@ -0,0 +1,14 @@
+package winlyps.noVinesSpread
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoVinesSpread : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
